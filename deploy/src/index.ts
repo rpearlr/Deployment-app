98a4b83c85549030ec32733d598c0721ba530e84
rpearlr <140599004+rpearlr@users.noreply.github.com>
2025-11-27 22:33:17 +0530

    Refactor main function

--- deploy/src/index.ts
+++ deploy/src/index.ts
@@ -1,27 +1,33 @@
+const { createClient, commandOptions } = require("redis");
+const { copyFinalDist, downloadS3Folder } = require("./aws");
+const { buildProject } = require("./utils");
 
-import { createClient, commandOptions } from "redis";
-import { copyFinalDist, downloadS3Folder } from "./aws";
-import { buildProject } from "./utils";
 const subscriber = createClient();
-subscriber.connect();
-
 const publisher = createClient();
-publisher.connect();
 
-async function main() {
-    while(1) {
-        const res = await subscriber.brPop(
-            commandOptions({ isolated: true }),
-            'build-queue',
-            0
-          );
-        // @ts-ignore;
-        const id = res.element
-        
-        await downloadS3Folder(`output/${id}`)
-        await buildProject(id);
-        copyFinalDist(id);
-        publisher.hSet("status", id, "deployed")
+async function start() {
+  await subscriber.connect();
+  await publisher.connect();
+
+  while (true) {
+    try {
+      const res = await subscriber.brPop(
+        commandOptions({ isolated: true }),
+        "build-queue",
+        0
+      );
+
+      const id = res?.element || res?.message || res?.value;
+      if (!id) continue;
+
+      await downloadS3Folder(`output/${id}`);
+      await buildProject(id);
+      await copyFinalDist(id);
+      await publisher.hSet("status", id, "deployed");
+    } catch (err) {
+      console.error(err);
     }
+  }
 }
-main();
+
+start();
